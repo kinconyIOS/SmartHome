d688c85aed84b5e678edc653c1091e410b655cd1
sunzl <sunzl@sunzldeimac.local>
2015-12-15 11:14:25 +0800

    sss

--- SmartHome/Sections/Home/HomeVC.swift
+++ SmartHome/Sections/Home/HomeVC.swift
@@ -17,7 +17,7 @@ class HomeVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     func configView(){
-      
+        
         hscroll!.frame=CGRectMake(0,20, ScreenWidth-60, 44);
         self.navigationItem.titleView=hscroll;
         scrollAddBtn()
@@ -29,23 +29,26 @@ class HomeVC: UIViewController {
             let btn:UIButton=UIButton.init(frame: CGRectMake(0, 2, ScreenWidth/6, 30))
             //设置倒角
             btn.layer.cornerRadius=15;
+            btn.setTitleColor(UIColor.blackColor(),forState:UIControlState.Normal);
+            btn.backgroundColor=UIColor.clearColor()
             if index==1 {
                 btn.backgroundColor=UIColor.whiteColor();
                 btn.setTitleColor(UIColor.orangeColor(), forState: UIControlState.Normal)
             }
             btn.setTitle(array[index-1], forState: UIControlState.Normal)
             btn.tag=index
+            
             btn.titleLabel?.font=UIFont.systemFontOfSize(15)
             btn.addTarget(self,action: Selector("newTap:"), forControlEvents: UIControlEvents.TouchUpInside)
             hscroll?.addButton(btn, with: 45)
-         }
+        }
     }
     func newTap(btn:UIButton)
     {
-      hscroll?.clearColor();
-      btn.setTitleColor(UIColor.orangeColor(), forState: UIControlState.Normal)
-      btn.backgroundColor=UIColor.whiteColor();
-    // selectMenuId=(int)btn.tag-1;
+        hscroll?.clearColor();
+        btn.setTitleColor(UIColor.orangeColor(), forState: UIControlState.Normal)
+        btn.backgroundColor=UIColor.whiteColor();
+        // selectMenuId=(int)btn.tag-1;
     }
 }
 
